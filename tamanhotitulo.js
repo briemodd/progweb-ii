e38438d37a4a12158a230b2ab738709caf457120
briemodd <101676097+briemodd@users.noreply.github.com>
2022-09-22 15:46:53 -0300

    Create tamanhotitulo.js

---
+++ tamanhotitulo.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', pc_h);
+let fontSize = 100
+let increaseDecrease = 10
+let elementBody = document.querySelector('body');
+let a = 1;
+
+function pc_h(){
+	ech = document.querySelector(`h${a}`);
+	if(ech != null){
+		criarBotao();
+	} else if (ech == null && a < 7) {
+		a += 1;
+		pc_h();
+	}
+}
+
+function criarBotao(){
+	botao = document.createElement('button');
+	botao.innerHTML = '+';
+	botao.addEventListener('click',function(){
+	
+	});
+	st = document.querySelector(`h${a}`);
+	st.before(botao);
+	
+	botao.addEventListener('click', function(event) {
+		if(fontSize <= 170){
+			fontSize = fontSize + increaseDecrease;
+			elementBody.style.fontSize = fontSize + '%';
+		}
+	});	
+	
+	botaom = document.createElement('button');
+	botaom.innerHTML = '-';
+	botaom.addEventListener('click',function(){
+	});
+
+	st1 = document.querySelector(`h${a}`);
+	st1.before(botaom);
+	
+	botaom.addEventListener('click', function(event) {
+		if(fontSize > 100){
+			fontSize = fontSize - increaseDecrease;
+			elementBody.style.fontSize = fontSize + '%';
+		}
+	});
+	
+	
+}
